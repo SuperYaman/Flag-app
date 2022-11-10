89f83d9755daadeb34f2800aff20b5e4c8b969f2
SuperYaman <aimane.gaillet@gmail.com>
2022-11-10 14:22:26 +0100

    react app flag country

---
+++ flag-app/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+        <Logo />
+        <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo modi culpa sequi quidem earum quos, ab aperiam eveniet pariatur perspiciatis illum magni alias iure dolor obcaecati facere architecto fugit? In exercitationem, quidem obcaecati minus beatae distinctio debitis architecto? Quidem aperiam dolorem facere voluptas. Ab praesentium dolores illum ratione quod et, aspernatur, perspiciatis atque eos dolorum laboriosam, saepe quas ex voluptas minima tempore odio ea! Maiores voluptates saepe neque dolorem? Aliquam, nisi ipsam excepturi porro rerum unde voluptates aperiam dolore! Velit, voluptates reiciendis molestias deleniti ducimus sunt tempora ipsa. Laborum tenetur adipisci praesentium ab omnis fugit assumenda explicabo cumque consectetur, odio quis similique tempora consequuntur nulla, facilis officiis nam nisi quam corporis illo. Eum porro vero dolorum illo magni, ullam laborum voluptatum quasi cupiditate atque ut iure ea unde tempore explicabo beatae adipisci. Voluptas optio, laboriosam atque et quam dolores blanditiis nostrum, culpa ipsum beatae, facere adipisci quas nam. Doloremque voluptas molestias sed animi sequi quas ut placeat consectetur culpa inventore, perferendis quasi porro pariatur delectus ullam quod nostrum aspernatur ratione minima sint, repellat laboriosam aut molestiae! Eius expedita praesentium vel beatae. Fuga ut ipsum illum eaque ad, vero incidunt consectetur unde inventore culpa ducimus hic? Architecto nisi blanditiis aliquam amet.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis rerum, neque tempora esse debitis repellat dignissimos aperiam, quis officia ipsam molestias, labore nemo culpa earum quam modi sapiente exercitationem corporis doloribus dolorem accusamus quae quas. Id temporibus impedit maxime fuga, dolores eum esse distinctio officiis necessitatibus debitis, eligendi expedita sapiente sint modi soluta fugiat magni, reprehenderit enim? Necessitatibus quas reiciendis iusto atque placeat! Velit officia obcaecati eum. Dignissimos, perspiciatis tempore? Magni, eveniet labore, ipsum blanditiis enim velit facilis nulla odit sunt et ullam, ad laudantium beatae rem omnis nobis totam non? Suscipit id laboriosam consectetur in aspernatur explicabo quod laborum odio, ducimus nemo ullam labore. Eius maiores, dolorem cum reprehenderit aliquid sunt sed est architecto ad nostrum tenetur pariatur corporis! Delectus, a. Tenetur repellat earum deleniti dolor magni itaque dolores incidunt eligendi iure vel facere voluptatum aspernatur beatae doloribus velit libero quae nisi, blanditiis aliquid illo natus labore maxime. Laudantium ad placeat molestias libero dolorem. Iste sunt quasi vel, distinctio aperiam amet. Consequatur excepturi voluptates numquam, voluptate aliquid suscipit voluptatum temporibus unde, magni ratione aperiam ipsa! Quos atque distinctio nesciunt libero animi possimus aut perferendis cum. A, qui animi, aspernatur corrupti eaque sed possimus officiis facere, cumque praesentium ut provident!</p>
+        </div>
+    );
+};
+
+export default About;
